1abf11244469d4ce49ac292cf51ef22066eda87e
Péter Kalicz <kaliczp@gmail.com>
2017-04-04 13:14:19 +0200

    Lat lon

--- process.R
+++ process.R
@@ -1,3 +1,8 @@
 LAI <- read.csv("LAIday.csv")
 library(xts)
 LAI.xts <- xts(LAI[,2], as.Date(LAI[,1]))
+library(ncdf4)
+P.nc <- nc_open("fresee2.1_prec.nc")
+Lon <- ncvar_get(P.nc, "Lon")
+Lat <- ncvar_get(P.nc, "Lat")
+
